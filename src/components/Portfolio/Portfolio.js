b9894eb07407214b2aa398fa2a4ced382767b7e5
Sergei Shabanov <shabanov1989@gmail.com>
2022-08-24 14:28:03 +0300

    fix: lang

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -9,19 +9,19 @@ function Portfolio() {
             <h2 className="portfolio__title">Портфолио</h2>
           </div>
           <ul className="portfolio__project-container">
-            <li>
+            <li className="portfolio__project-list-wrapper">
               <a href="https://sergeishabanov1989.github.io/russian-travel/" className="portfolio__project-wrapper" target="_blank">
                 <p className="portfolio__project">Статичный сайт</p>
                 <img src={arrow} className="portfolio__project__link" alt="здесь должны быть стрелочка"/>
               </a>
             </li>
-            <li>
+            <li className="portfolio__project-list-wrapper">
               <a href="https://sergeishabanov1989.github.io/russian-travel/" className="portfolio__project-wrapper" target="_blank">
                 <p className="portfolio__project">Адаптивный сайт</p>
                 <img src={arrow} className="portfolio__project__link" alt="здесь должны быть стрелочка"/>
               </a>
             </li>
-            <li>
+            <li className="portfolio__project-list-wrapper">
               <a href="https://mesto.sergei-shabanov.nomoredomains.xyz/signin" className="portfolio__project-wrapper" target="_blank">
                 <p className="portfolio__project">Одностраничное приложение</p>
                 <img src={arrow} className="portfolio__project__link" alt="здесь должны быть стрелочка"/>
